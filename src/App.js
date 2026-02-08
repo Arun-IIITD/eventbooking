d877b7c05761a068117b2b92b075f3412ac969f0
Arun Yadav <ay20552291@gmail.com>
2026-02-08 20:07:32 +0530

    fourth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home/Home";
-import EventCard from "./components/EventCard/EventCard";
 import MyBookings from "./MyBookings/MyBookings";
 import Navbar from "./components/NavBar/NavBar";
 import Events from "./components/Events/Events";
